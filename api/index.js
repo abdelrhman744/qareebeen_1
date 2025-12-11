a310c4eec74fcbc9467fcc8cc6e784acde5acaaf
abdelrhman744 <abdelrhmanhany912@gmail.com>
2025-12-11 21:53:55 +0200

    add files

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index");
+    res.render("index");
 });
 
 const port = process.env.PORT || 3000;
